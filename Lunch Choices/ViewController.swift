8278501b333da97ec8bb11ce3a4c6e76a8c219ba
Jessica Olivieri <jessicaolivieri@jessicas-macbook-pro.local>
2018-09-26 14:44:38 -0400

    1.1; Lunch Choices - working with conditionals

--- Lunch Choices/ViewController.swift
+++ Lunch Choices/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	// 3 switches
+	@IBOutlet weak var peanutAllergy: UISwitch!
+	@IBOutlet weak var glutenAllergy: UISwitch!
+	@IBOutlet weak var vegetarian: UISwitch!
+	
+	//food image and label
+	@IBOutlet weak var choiceImage: UIImageView!
+	@IBOutlet weak var choiceLabel: UILabel!
+	
+	var lunchChoices = ["Garden Salad", "PB_J Sandwich", "Roast Beef Sandwich"]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
 
-
+	@IBAction func showChoicesPressed(_ sender: UIButton) {
+		var choice = ""
+		if !vegetarian.isOn && !glutenAllergy.isOn {
+			choice = "Roast Beef Sandwich"
+		} else if vegetarian.isOn && peanutAllergy.isOn {
+			choice = "Garden Salad"
+		} else {
+			choice = "PB_J Sandwich"
+		}
+		choiceLabel.text = choice
+		choiceImage.image = UIImage(named: choice)
+	}
+	
 }
 
